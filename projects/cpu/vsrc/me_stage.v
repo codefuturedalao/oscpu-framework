2a09df9ed137ab89d17c8251ea307ea2f5b9b2d5
tracer-oscpu2021 <tracer@oscpu.org>
2021-08-21 05:41:03 -0700

    桑乾龙 20210692    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: 4685c05a4f634522baf10812aca97aab            Boot ID: 5fe92ccdd6e340e0af3ac1ae6e7f0aab     Virtualization: vmware   Operating System: Ubuntu 20.04.2 LTS             Kernel: Linux 5.11.0-27-generic       Architecture: x86-64  05:41:03 up 23:49,  1 user,  load average: 0.43, 0.27, 0.26

--- projects/cpu/vsrc/me_stage.v
+++ projects/cpu/vsrc/me_stage.v
@@ -32,9 +32,15 @@ assign mem_data_read_o = mem_data_read_i;
 assign mem_rvalid =  me_mem_rena & ~mem_rready & ~me_exception_flag & ~wb_exception_flag;
 assign mem_wvalid = me_mem_wena  & ~mem_wready & ~me_exception_flag & ~wb_exception_flag;
 //assign mem_req = (me_mem_rena & `REQ_READ) | (me_mem_wena & `REQ_WRITE);
-assign mem_rsize = ({2{(mem_byte_enable == 8'b0000_0001)}} & `SIZE_B);
-assign mem_wsize = ({2{(mem_byte_enable == 8'b0000_0001)}} & `SIZE_B);
+assign mem_rsize = ({2{(mem_byte_enable == 8'b0000_0001)}} & `SIZE_B)
+				| ({2{(mem_byte_enable == 8'b0000_0011)}} & `SIZE_H)
+				| ({2{(mem_byte_enable == 8'b0000_1111)}} & `SIZE_W)
+				| ({2{(mem_byte_enable == 8'b1111_1111)}} & `SIZE_D);
 
+assign mem_wsize = ({2{(mem_byte_enable == 8'b0000_0001)}} & `SIZE_B)
+				| ({2{(mem_byte_enable == 8'b0000_0011)}} & `SIZE_H)
+				| ({2{(mem_byte_enable == 8'b0000_1111)}} & `SIZE_W)
+				| ({2{(mem_byte_enable == 8'b1111_1111)}} & `SIZE_D);
 
 assign stall_req =  (mem_rvalid & ~mem_rready) | (mem_wvalid & ~mem_wready);
 				
