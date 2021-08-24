68912c76b28e0829d0caed82eca28a6187e429b2
tracer-oscpu2021 <tracer@oscpu.org>
2021-08-23 19:05:49 -0700

    桑乾龙 20210692    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: 4685c05a4f634522baf10812aca97aab            Boot ID: 48f7ee9b236e4c839972b052950f2a54     Virtualization: vmware   Operating System: Ubuntu 20.04.2 LTS             Kernel: Linux 5.11.0-27-generic       Architecture: x86-64  19:05:49 up 34 min,  1 user,  load average: 0.56, 0.25, 0.16

--- projects/cpu/vsrc/hazard_unit.v
+++ projects/cpu/vsrc/hazard_unit.v
@@ -105,7 +105,8 @@ module hazard_unit(
 													(control_transfer_r & ~if_stall_req)? {`STALL_NEXT, `STALL_ZERO, `STALL_ZERO, `STALL_ZERO, `STALL_ZERO} :	//control hazard
 
 																		mem_stall_req? {`STALL_KEEP, `STALL_KEEP, `STALL_KEEP, `STALL_KEEP, `STALL_KEEP} :	//load and store
-																		exe_stall_req? {`STALL_KEEP, `STALL_KEEP, `STALL_KEEP, `STALL_ZERO, `STALL_NEXT} :	//mul and div
+																		//exe_stall_req? {`STALL_KEEP, `STALL_KEEP, `STALL_KEEP, `STALL_ZERO, `STALL_NEXT} :	//mul and div
+																		exe_stall_req? {`STALL_KEEP, `STALL_KEEP, `STALL_KEEP, `STALL_KEEP, `STALL_KEEP} :	//mul and div
 																		id_stall_req ? {`STALL_KEEP, `STALL_KEEP, `STALL_ZERO, `STALL_NEXT, `STALL_NEXT} :	//data hazard
 																		if_stall_req ? {`STALL_KEEP, `STALL_ZERO, `STALL_NEXT, `STALL_NEXT, `STALL_NEXT} :	//fetch inst
 																					   {`STALL_NEXT, `STALL_NEXT, `STALL_NEXT, `STALL_NEXT, `STALL_NEXT} ;	//default
